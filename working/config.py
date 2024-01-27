e3ef803af7b81bbb3d461e1a9971c48c182e6bbe
dalimsadek <mohamedali.msadek@supcom.tn>
2024-01-27 18:27:13 +0100

    ty haya

--- working/config.py
+++ working/config.py
@@ -3,5 +3,5 @@ no_of_classes = 7
 picture_size = 48
 batch_size  = 128
 learning_rate = 0.0001
-folder_path = r"C:\Users\Dali\Desktop\Ai Project Github\Emotion Recognition Using CNN\data\images\images\\"
+folder_path = r"..\data\images\images\\"
 epochs = 48
